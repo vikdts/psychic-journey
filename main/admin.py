428e33517496966e650920d19ebf517b65d39c02
vikdts <v.d.tsareva@gmail.com>
2025-12-04 11:34:29 +0000

    add inline prooject image field

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,10 @@ from .models import Tag, Project, ProjectImage
 
 # Register your models here.
 
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'link')
     inlines = [] 
