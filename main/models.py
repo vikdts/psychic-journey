9c90b9ab470dabc2ac37659a0a39f1e401f41e5b
vikdts <v.d.tsareva@gmail.com>
2025-12-04 10:41:41 +0000

    create project image model

--- main/models.py
+++ main/models.py
@@ -17,4 +17,9 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='project_images/')
 
+    def __str__(self):
+        return f"Image for {self.project.title}" 
