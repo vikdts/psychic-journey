28b959faede638b6a5ad16e0d11699a6fa17b6a0
vikdts <v.d.tsareva@gmail.com>
2025-12-04 11:24:04 +0000

    add project admin model

--- main/admin.py
+++ main/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Tag, Project, ProjectImage
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'link')
+    inlines = [] 
+    search_fields = ('title', 'description')
+    list_filter = ('tags',)
